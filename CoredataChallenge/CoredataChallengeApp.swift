ac0e4abaafb1b54503213b5c51eca1dd269cf1b3
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2023-08-31 23:20:49 -0400

    Initial Commit

---
+++ CoredataChallenge/CoredataChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoredataChallengeApp.swift
+//  CoredataChallenge
+//
+//  Created by Venkatesh Nyamagoudar on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CoredataChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
